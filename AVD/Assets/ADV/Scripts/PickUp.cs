39e871810f970678de475a7a85ad44b6a4416f83
Iban D <delgadoiban1a@gmail.com>
2020-02-28 17:29:01 +0100

    sunnyland finished

--- AVD/Assets/ADV/Scripts/PickUp.cs
+++ AVD/Assets/ADV/Scripts/PickUp.cs
@@ -2,20 +2,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = System.Random;
 
+
+public enum ItemType
+{
+    Gem,
+    Cherry
+}
 public class PickUp : MonoBehaviour
 {
-
-    public static event Action OnPickUp;
+    public static event Action<int> OnPickUp;
     private Animator anim;
     private BoxCollider2D col;
+    public ItemType itemType = ItemType.Gem;
+    private AudioSource source;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
         col = GetComponent<BoxCollider2D>();
+        source = GetComponent<AudioSource>();
         col.enabled = true;
+        RandomizeItem();
+        String item = "";
+        switch (itemType)
+        {
+            case ItemType.Gem:
+                item = "gem";
+                break;
+            case ItemType.Cherry:
+                item = "cherry";
+                break;
+        }
+        anim.SetTrigger(item);
+    }
+
+    private void RandomizeItem()
+    {
+        float chance = UnityEngine.Random.Range(0, 100);
+        if (chance <= 50)
+        {
+            itemType = ItemType.Gem;
+        }
+        else
+        {
+            itemType = ItemType.Cherry;
+        }
     }
 
     private void Start()
@@ -32,8 +64,23 @@ public class PickUp : MonoBehaviour
             collided = true;
             col.enabled = false;
             anim.SetTrigger("pickUp");
-            OnPickUp?.Invoke();
+            source.Play();
+            switch (itemType)
+            {
+                case ItemType.Gem:
+                    OnPickUp?.Invoke(1);
+                    break;
+                case ItemType.Cherry:
+                    OnPickUp?.Invoke(2);
+                    break;
+            }
             Destroy(gameObject, 0.267f);
         }
     }
 }
+
+
+
+
+
+
--- AVD/Assets/ADV/Scripts/PlayerShooter2D.cs
+++ AVD/Assets/ADV/Scripts/PlayerShooter2D.cs
@@ -2,16 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerShooter2D : MonoBehaviour
 {
     public Projectile projectile;
 
     private PlayerController2D playerController;
+    private AudioSource source;
     public Transform gunMuzzle;
     private void Awake()
     {
         playerController = GetComponent<PlayerController2D>();
+        source = GetComponent<AudioSource>();
     }
 
 
@@ -21,6 +24,8 @@ public class PlayerShooter2D : MonoBehaviour
         {
             var bullet = Instantiate(projectile, gunMuzzle.position, Quaternion.identity);
             bullet.SetDirection(playerController.FacingRight);
+            source.pitch = Random.Range(0.9f, 1.1f);
+            source.Play();
         }
     }
 }
