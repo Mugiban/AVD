b3e73a07f2a22ccf6ff7b9a80b889e704d796dc3
Iban <delgadoiban1a@gmail.com>
2020-03-25 16:22:38 +0100

    nav mesh movement and cinemachine

---
+++ AVD/Assets/ADV/Scripts/NavMeshScene/NPCMovement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCMovement : MonoBehaviour
+{
+    private NavMeshAgent navMesh;
+    private Rigidbody rb;
+    private Animator anim;
+
+    private void Awake()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hitInfo))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                MoveToPosition(hitInfo.point);
+            }
+        }
+
+        if (navMesh.hasPath && navMesh.remainingDistance < 0.05f)
+        {
+            anim.SetBool("moving", false);
+        }
+        
+    }
+
+    private void MoveToPosition(Vector3 position)
+    {
+        anim.SetBool("moving", true);
+        navMesh.SetDestination(position);
+    }
+}
