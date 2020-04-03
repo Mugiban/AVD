6b803b5ba7e5a87974de5c0b2aac6c79b76ba499
Iban D <delgadoiban1a@gmail.com>
2020-04-03 23:29:22 +0200

    open chest, animation, detection of player and timeline playing
    falta animar al jugador y la dolly camera

---
+++ AVD/Assets/ADV/Chest/Chest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    private Animator anim;
+    private TimelineController timelineController;
+    private float stayTimer = 0;
+    [SerializeField] private float stayTime = 1f;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        timelineController = GetComponent<TimelineController>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            stayTimer = 0;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            stayTimer += Time.deltaTime;
+            if (stayTimer >= stayTime)
+            {
+                OpenChest();
+            }
+        }
+    }
+
+    void OpenChest()
+    {
+        //anim.SetTrigger("open");
+       timelineController.Play();
+    }
+}
